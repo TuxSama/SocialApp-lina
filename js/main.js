f61e9943dd91cb60738d3a1278eff0b636246401
Amine Dounia <amindonya806@gmail.com>
2025-04-28 13:20:02 +0100

    rak fahem

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 splash=document.getElementById("splash");
 splash.onload=setTimeout(function redirect(){
-    window.location.href="inscription.html"
+    window.location.href="/./inscription.html"
 },5000);
